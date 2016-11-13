92818c6b891b89dca862438d42ffa7e05991f2ff
bayramgeldi <bayramgeldi@MacBookPro.local>
2016-11-14 00:39:37 +0300

    font türü, padding, alignment yapıdı

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>IETT Akbil gidereri hesaplama</title>
+	<title>IETT kart gideri hesaplama hesaplama</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 <body>
+<table>
+	<tr>
+		<td><img src="images/anonim-karttur.png"><br>Anonim Kart</td>
+		<td><img src="images/mavi-kartlartur.png"><br>Mavi Kart</td>
+		<td><img src="images/indirimli-karttur.png"><br>İndirimli Kart</td>
+		<td><img src="images/ucretsiz-karttur.png"><br>Ücretsiz Kart</td>
+	</tr>
 
+</table>
 </body>
 </html>
