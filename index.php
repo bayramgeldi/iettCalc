37d3c28586c2fac921749a577552ac7a0b51941c
bayramgeldi <bayramgeldi@MacBookPro.local>
2016-11-14 01:00:39 +0300

    en güncel hale getirdim

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
 	<link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 <body>
+<h1>Akbil türünü seçiniz:</h1>
 <table>
 	<tr>
-		<td><img src="images/anonim-karttur.png"><br>Anonim Kart</td>
+		<td><a href=""></a><img src="images/anonim-karttur.png"><br>Anonim Kart</td>
 		<td><img src="images/mavi-kartlartur.png"><br>Mavi Kart</td>
 		<td><img src="images/indirimli-karttur.png"><br>İndirimli Kart</td>
 		<td><img src="images/ucretsiz-karttur.png"><br>Ücretsiz Kart</td>
