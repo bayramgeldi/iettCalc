4156b6a91c4a3af75f55a691fba327ec5fae7c63
bayramgeldi <bayramgeldi@MacBookPro.local>
2016-11-15 14:09:34 +0300

    imagelere radio button özelliği verildi
    calc.php ve disp_form sonra silinecek

--- index.php
+++ index.php
@@ -12,34 +12,41 @@
 		<div><img src="images/indirimli-karttur.png"><br>İndirimli Kart</div>
 		<div><img src="images/ucretsiz-karttur.png"><br>Ücretsiz Kart</div> -->
 <div id="hepsi">
+	<label class="radio">
 	<div class="img">
-  <a href="#">
+	<input type="radio" name="fb" value="Anon" />
     <img src="images/anonim-karttur.png" alt="Anonim Kart" width="300" height="200">
-  </a>
+  
   <div class="desc">Anonim Kart</div>
 </div>
+</label>
 
+<label class="radio">
 <div class="img">
-  <a href="#">
-    <img src="images/mavi-kartlartur.png" alt="Mavi Kart" width="600" height="200">
-  </a>
+<input type="radio" name="fb" value="mavi" />
+  <img src="images/mavi-kartlartur.png" alt="Mavi Kart" width="600" height="200">
   <div class="desc">Mavi Kart</div>
 </div>
+</label>
 
+<label class="radio">
 <div class="img">
-  <a href="#">
+<input type="radio" name="fb" value="indr" />
     <img src="images/indirimli-karttur.png" alt="İndirimli Kart" width="600" height="200">
-  </a>
+  
   <div class="desc">İndirimli Kart</div>
 </div>
+</label>
 
+<label class="radio">
 <div class="img">
-  <a href="#">
+<input type="radio" name="fb" value="ucr" />
     <img src="images/ucretsiz-karttur.png" alt="Ücretsiz Kart" width="600" height="200">
-  </a>
+  
   <div class="desc">Ücretsiz Kart</div>
 </div>
 </div>
+</label>
 
 </body>
 </html>
