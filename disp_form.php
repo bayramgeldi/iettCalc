d1878ca78ad3689c30205b379680452f6912a502
bayramgeldi <bayramgeldi@MacBookPro.local>
2016-11-15 15:18:24 +0300

    şuanlık radio'dan veri çekemiyor

--- disp_form.php
+++ disp_form.php
@@ -1,16 +1,26 @@
+<?php
+
+?>
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>Answer</title>
+<title>Toplam Masraf</title>
+<link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 <body>
-<p>The answer is: 
+<p>Toplam Masraf: 
 <?php
-$first = $_POST['first'];
-$second= $_POST['second'];
-if($_POST['group1'] == 'add') {
-echo $first + $second;
+$first = $_POST['aktarma'];
+
+//post'tan veri çekemiyor
+
+
+
+
+
+if($_POST['Akbil'] == 'anon') {
+echo $first + 2;
 }
 else if($_POST['group1'] == 'subtract') {
 echo $first - $second;
--- index.php
+++ index.php
@@ -11,19 +11,19 @@
 		<div><img src="images/mavi-kartlartur.png"><br>Mavi Kart</div>
 		<div><img src="images/indirimli-karttur.png"><br>İndirimli Kart</div>
 		<div><img src="images/ucretsiz-karttur.png"><br>Ücretsiz Kart</div> -->
+<form method="post" attribute="post" action="disp_form.php">
 <div id="hepsi">
-	<label class="radio">
-	<div class="img">
-	<input type="radio" name="fb" value="Anon" />
+<label class="radio">
+<div class="img">
+<input type="radio" name="Akbil" value="Anon" />
     <img src="images/anonim-karttur.png" alt="Anonim Kart" width="300" height="200">
-  
-  <div class="desc">Anonim Kart</div>
+  	<div class="desc">Anonim Kart</div>
 </div>
 </label>
 
 <label class="radio">
 <div class="img">
-<input type="radio" name="fb" value="mavi" />
+<input type="radio" name="Akbil" value="mavi" />
   <img src="images/mavi-kartlartur.png" alt="Mavi Kart" width="600" height="200">
   <div class="desc">Mavi Kart</div>
 </div>
@@ -31,7 +31,7 @@
 
 <label class="radio">
 <div class="img">
-<input type="radio" name="fb" value="indr" />
+<input type="radio" name="Akbil" value="indr" />
     <img src="images/indirimli-karttur.png" alt="İndirimli Kart" width="600" height="200">
   
   <div class="desc">İndirimli Kart</div>
@@ -40,7 +40,7 @@
 
 <label class="radio">
 <div class="img">
-<input type="radio" name="fb" value="ucr" />
+<input type="radio" name="Akbil" value="ucr" />
     <img src="images/ucretsiz-karttur.png" alt="Ücretsiz Kart" width="600" height="200">
   
   <div class="desc">Ücretsiz Kart</div>
@@ -48,5 +48,16 @@
 </div>
 </label>
 
+<p class="aktarma">Aktarma sayısı:<br/>
+<input type="text" id="aktarma" name="aktarma"> <br>
+<button type="submit" name="answer" id="answer" value="answer">Calculate</button>
+
+</p>
+
+<p></p>
+
+
+
+</form>
 </body>
 </html>
