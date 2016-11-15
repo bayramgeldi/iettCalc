f6a08e7944fdb193617ad40a18e0bbefa2773833
bayramgeldi <bayramgeldi@MacBookPro.local>
2016-11-15 12:40:06 +0300

    yeni görünüm eklendi

--- index.php
+++ index.php
@@ -6,14 +6,40 @@
 </head>
 <body>
 <h1>Akbil türünü seçiniz:</h1>
-<table>
-	<tr>
-		<td><a href=""></a><img src="images/anonim-karttur.png"><br>Anonim Kart</td>
-		<td><img src="images/mavi-kartlartur.png"><br>Mavi Kart</td>
-		<td><img src="images/indirimli-karttur.png"><br>İndirimli Kart</td>
-		<td><img src="images/ucretsiz-karttur.png"><br>Ücretsiz Kart</td>
-	</tr>
 
-</table>
+		<!-- <div><img src="images/anonim-karttur.png"><br>Anonim Kart</div>
+		<div><img src="images/mavi-kartlartur.png"><br>Mavi Kart</div>
+		<div><img src="images/indirimli-karttur.png"><br>İndirimli Kart</div>
+		<div><img src="images/ucretsiz-karttur.png"><br>Ücretsiz Kart</div> -->
+<div id="hepsi">
+	<div class="img">
+  <a href="#">
+    <img src="images/anonim-karttur.png" alt="Anonim Kart" width="300" height="200">
+  </a>
+  <div class="desc">Anonim Kart</div>
+</div>
+
+<div class="img">
+  <a href="#">
+    <img src="images/mavi-kartlartur.png" alt="Mavi Kart" width="600" height="200">
+  </a>
+  <div class="desc">Mavi Kart</div>
+</div>
+
+<div class="img">
+  <a href="#">
+    <img src="images/indirimli-karttur.png" alt="İndirimli Kart" width="600" height="200">
+  </a>
+  <div class="desc">İndirimli Kart</div>
+</div>
+
+<div class="img">
+  <a href="#">
+    <img src="images/ucretsiz-karttur.png" alt="Ücretsiz Kart" width="600" height="200">
+  </a>
+  <div class="desc">Ücretsiz Kart</div>
+</div>
+</div>
+
 </body>
 </html>
